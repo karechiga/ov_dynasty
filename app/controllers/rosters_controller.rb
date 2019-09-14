bba7d5305ae8d7ff4e10fcf4e18c21c308cad1fc
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-09-14 02:47:14 +0000

    edit form allowing changes to team name, motto and abbreviation

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -1,25 +1,46 @@
 class RostersController < ApplicationController
   before_action :authenticate_user!
   before_action :require_user_membership
+  before_action :require_user_roster_owner, only: :edit
 
   def show
+    @roster = current_roster
     @positions = positions
-    @roster = Roster.find(params[:id])
     @players = @roster.players
     @picks = @roster.picks
     @player_associations = @roster.player_associations
     @season = current_season
     @years = years
   end
+
+  def edit
+    @roster = current_roster
+  end
+
+  def update
+    @roster = current_roster
+    @roster.update_attributes(roster_params)
+    redirect_to league_roster_path(current_league, current_roster)
+  end
   
   private
-  
+
+  def current_roster
+    @roster = Roster.find(params[:id])
+  end
+
   def current_league
     @current_league ||= League.find(params[:league_id])
   end
   
   def current_membership
     @current_membership = current_user.memberships.find_by_league_id(current_league)
+  end
+
+  def require_user_roster_owner
+    if !current_user.owns_roster?(current_roster)
+      render plain: "Unauthorized", status: :unauthorized
+    end
   end
   
   def require_user_membership
@@ -28,9 +49,7 @@ class RostersController < ApplicationController
     end
   end
   def roster_params
-    params.require(:roster).permit(:team_name, :team_abbrev,
-      :motto, :cap_space, :penalty, :traded_salary, :wins,
-      :losses, :ties)
+    params.require(:roster).permit(:team_name, :team_abbrev, :motto)
   end
 
   def current_season
@@ -60,4 +79,5 @@ class RostersController < ApplicationController
                   'DL', 'DL', 'DL', 'Stash',
                   'Stash']
   end
+
 end
