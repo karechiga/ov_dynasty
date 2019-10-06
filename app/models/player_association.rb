4ac4660a18fbb80ce8d0b258d64e43b07e5594b2
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-10-06 15:23:42 +0000

    roster salary updates on roster movement. Must refresh though to see change.

--- app/models/player_association.rb
+++ app/models/player_association.rb
@@ -3,4 +3,18 @@ class PlayerAssociation < ApplicationRecord
   belongs_to :roster_spot
 
   has_many :contract_years
+
+  before_update :update_roster_player_salary
+
+  def update_roster_player_salary
+    old_roster_spot = RosterSpot.find(roster_spot_id_was)
+    roster_spot.roster.roster_salaries.each do |roster_salary|
+      year = roster_salary.year
+      contract_year = contract_years.find_by_season(year)
+      if contract_year != nil
+        salary = roster_salary.player_salary_total - old_roster_spot.salary_multiplier * contract_year.salary + roster_spot.salary_multiplier * contract_year.salary
+        roster_salary.update(player_salary_total: salary)
+      end
+    end
+  end
 end
