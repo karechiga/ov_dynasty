8f1ef544eb11b8470b4fea5aeaceb83405f65eb3
Kyle Arechiga <kylearechiga@email.arizona.edu>
2020-01-27 17:14:55 +0000

    added links to each day of a matchup

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,15 @@
 class Day < ApplicationRecord
   belongs_to :matchup
+
+  def is_today?
+    time = Time.current
+    today = Date.today
+    yesterday = Date.yesterday
+    day_date = Date.parse(self.date)
+    if time.hour < 10
+      return day_date == yesterday
+    else
+      return day_date == today
+    end
+  end
 end
